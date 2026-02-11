660dd3be659c4b7976c21fe587153dc308868f40
markchappelow-design <markchappelow@gmail.com>
2026-02-10 21:02:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -232,6 +232,3 @@ if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=PORT)
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=PORT)
